7fae834f8f152edc7765b408efc501bca442bdf6
ericaminw <51761398+ericaminw@users.noreply.github.com>
2019-06-26 10:22:06 -0400

    Update plotp0s.m

--- plotp0s.m
+++ plotp0s.m
@@ -9,7 +9,7 @@ for i = 1:3
     % Plot start p0s by error
     if ~isempty(straightstart)
         hold on
-        figure(2)
+        figure(1)
         straightstartplot = plot3(straightstart(:,1),...
             straightstart(:,2),straightstart(:,3), 'bo')
         title('Straight start p0s')
@@ -17,7 +17,7 @@ for i = 1:3
     end
     if ~isempty(bvpfailstart)
         hold on
-        figure(3)
+        figure(2)
         bvpstartplot = plot3(bvpfailstart(:,1),bvpfailstart(:,2),...
             bvpfailstart(:,3), 'go')
         title('BVP solver fail start p0s')
@@ -25,7 +25,7 @@ for i = 1:3
     end
     if ~isempty(lsfailstart)
         hold on
-        figure(4)
+        figure(3)
         lsfstartplot = plot3(lsfailstart(:,1),lsfailstart(:,2),...
             lsfailstart(:,3), 'ro')
         title('Line search fail start p0s')
@@ -33,7 +33,7 @@ for i = 1:3
     end
     if ~isempty(noerrstart)
         hold on
-        figure(5)
+        figure(4)
         noerrstartplot = plot3(noerrstart(:,1),noerrstart(:,2),...
             noerrstart(:,3), 'mo')
         title('No error, not straight start p0s')
@@ -42,7 +42,7 @@ for i = 1:3
     % Plot end p0s by error
     if ~isempty(straightend)
         hold on
-        figure(6)
+        figure(5)
         straightendplot = plot3(straightend(:,1),straightend(:,2),...
             straightend(:,3), 'bo')
         title('Straight end p0s')
@@ -50,7 +50,7 @@ for i = 1:3
     end
     if ~isempty(bvpfailend)
         hold on
-        figure(7)
+        figure(6)
         bvpendplot = plot3(bvpfailend(:,1),bvpfailend(:,2),...
             bvpfailend(:,3), 'go')
         title('BVP solver fail end p0s')
@@ -58,7 +58,7 @@ for i = 1:3
     end
     if ~isempty(lsfailend)
         hold on
-        figure(8)
+        figure(7)
         lsfendplot = plot3(lsfailend(:,1),lsfailend(:,2),...
             lsfailend(:,3), 'ro')
         title('Line search fail end p0s')
@@ -66,7 +66,7 @@ for i = 1:3
     end
     if ~isempty(noerrend)
         hold on
-        figure(9)
+        figure(8)
         noerrendplot = plot3(noerrend(:,1),noerrend(:,2),...
             noerrend(:,3), 'mo')
         title('No error, not straight end p0s')
