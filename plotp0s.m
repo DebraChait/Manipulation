aca88d23887a6479e41ad37d8c42cd0665d0a506
DebraChait <33737987+DebraChait@users.noreply.github.com>
2019-06-27 13:24:30 -0400

    Plots p0s in 2 plots, simplified code

--- plotp0s.m
+++ plotp0s.m
@@ -1,93 +1,7 @@
 function output = plotp0s
 % Load the data from files and make major plots
-
-
-for i = 1:3
-    % Load the data
-    filename = sprintf('plotp0_%i',i)
-    load(filename)
-    % Plot start p0s, colored by error
-    if ~isempty(straightstart)
-        hold on
-        figure(1)
-        straightstartplot = plot3(straightstart(:,1),...
-            straightstart(:,2),straightstart(:,3), 'bo')
-        title('Starting p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart',...
-            'Location', 'Best')
-        hold off
-    end
-    if ~isempty(bvpfailstart)
-        hold on
-        figure(1)
-        bvpstartplot = plot3(bvpfailstart(:,1),bvpfailstart(:,2),...
-            bvpfailstart(:,3), 'go')
-        title('Starting p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart',...
-            'Location', 'Best')
-        hold off
-    end
-    if ~isempty(lsfailstart)
-        hold on
-        figure(1)
-        lsfstartplot = plot3(lsfailstart(:,1),lsfailstart(:,2),...
-            lsfailstart(:,3), 'ro')
-        title('Starting p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart',...
-            'Location', 'Best')
-        hold off
-    end
-    if ~isempty(noerrstart)
-        hold on
-        figure(1)
-        noerrstartplot = plot3(noerrstart(:,1),noerrstart(:,2),...
-            noerrstart(:,3), 'mo')
-        title('Starting p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart',...
-            'Location', 'Best')
-        hold off
-    end
     
-    % Plot end p0s, colored by error
-    if ~isempty(straightend)
-        hold on
-        figure(2)
-        straightendplot = plot3(straightend(:,1),straightend(:,2),...
-            straightend(:,3), 'bo')
-        title('Ending p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart')
-        hold off
-    end
-    if ~isempty(bvpfailend)
-        hold on
-        figure(2)
-        bvpendplot = plot3(bvpfailend(:,1),bvpfailend(:,2),...
-            bvpfailend(:,3), 'go')
-        title('Ending p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart')
-        hold off
-    end
-    if ~isempty(lsfailend)
-        hold on
-        figure(2)
-        lsfendplot = plot3(lsfailend(:,1),lsfailend(:,2),...
-            lsfailend(:,3), 'ro')
-        title('Ending p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart')
-        hold off
-    end
-    if ~isempty(noerrend)
-        hold on
-        figure(2)
-        noerrendplot = plot3(noerrend(:,1),noerrend(:,2),...
-            noerrend(:,3), 'mo')
-        title('Ending p0''s')
-        legend('straightstart','bvpfailstart','lsfailstart', 'noerrstart')
-        hold off
-    end
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Initialize data arrays    
 straightstartdata = [];
 bvpfailstartdata = [];
 lsfailstartdata = [];
@@ -98,37 +12,70 @@ lsfailenddata = [];
 noerrenddata = [];
 
 for i = 1:3
-    i
+    
     % Load the data
     filename = sprintf('plotp0_%i',i)
     load(filename) 
     
-    if ~isempty(straightstart)
-        straightstartdata = [straightstartdata; straightstart];
-    end
-    if ~isempty(bvpfailstart)
-        bvpfailstartdata = [bvpfailstartdata; bvpfailstart];
-    end
-    if ~isempty(lsfailstart)
-        lsfailstartdata = [lsfailstartdata; lsfailstart];
-    end
-    if ~isempty(noerrstart)
-        noerrstartdata = [noerrstartdata; noerrstart];
-    end
-    if ~isempty(straightend)
-        straightenddata = [straightenddata; straightend];
-    end
-    if ~isempty(bvpfailend)
-        bvpfailenddata = [bvpfailenddata; bvpfailend];
-    end
-    if ~isempty(lsfailend)
-        lsfailenddata = [lsfailenddata; lsfailend];
-    end
-    if ~isempty(noerrend)
-        noerrenddata = [noerrenddata; noerrend];
-    end
+    straightstartdata = [straightstartdata; straightstart];
+    bvpfailstartdata = [bvpfailstartdata; bvpfailstart];
+    lsfailstartdata = [lsfailstartdata; lsfailstart];
+    noerrstartdata = [noerrstartdata; noerrstart];
+    straightenddata = [straightenddata; straightend];
+    bvpfailenddata = [bvpfailenddata; bvpfailend];
+    lsfailenddata = [lsfailenddata; lsfailend];
+    noerrenddata = [noerrenddata; noerrend];
 
 end
 
+% Plot the start p0s, colored by error
+view(3)
+figure(1)
+hold on
+if ~isempty(bvpfailstartdata)
+plot3(bvpfailstartdata(:,1), bvpfailstartdata(:,2), ...
+    bvpfailstartdata(:,3), 'or')
+end
+if ~isempty(noerrstartdata)
+plot3(noerrstartdata(:,1),noerrstartdata(:,2),...
+             noerrstartdata(:,3), 'ob')
+end
+if ~isempty(straightstartdata)
+plot3(straightstartdata(:,1),...
+             straightstartdata(:,2),straightstartdata(:,3), 'og')
+end
+if ~isempty(lsfailstartdata)
+plot3(lsfailstartdata(:,1),lsfailstartdata(:,2),...
+             lsfailstartdata(:,3), 'oy')
+end
+title('start p0s')
+legend('bvpfailstart','noerrstart','straightstart',...
+    'lsfailstart', 'Location', 'Best')
 
+% Plot the end p0s, colored by error
+view(3)
+figure(2)
+hold on
+if ~isempty(bvpfailenddata)
+plot3(bvpfailenddata(:,1), bvpfailenddata(:,2), ...
+    bvpfailenddata(:,3), 'or')
+end
+if ~isempty(noerrenddata)
+plot3(noerrenddata(:,1),noerrenddata(:,2),...
+             noerrenddata(:,3), 'ob')
+end
+if ~isempty(straightenddata)
+plot3(straightenddata(:,1),...
+             straightenddata(:,2),straightenddata(:,3), 'og')
+end
+if ~isempty(lsfailenddata)
+plot3(lsfailenddata(:,1),lsfailenddata(:,2),...
+             lsfailenddata(:,3), 'oy')
+end
+title('end p0s')
+legend('bvpfailend','noerrend','straightend',...
+    'lsfailend', 'Location', 'Best')
+
+
+% End of function
 end
