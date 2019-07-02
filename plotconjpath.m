1feec2c8760cea36d7c49ee2d7e50049b719efa8
DebraChait <33737987+DebraChait@users.noreply.github.com>
2019-07-02 11:02:47 -0400

    Plots all p0 paths with conj pts from file

--- plotconjpath.m
+++ plotconjpath.m
@@ -3,27 +3,52 @@ function output = plotconjpath
 % Goal: plot path of one rod's p0s from start through to end
 % Differentiate between stable and unstable p0 locations
 
-% Load the data method 1
-% filename = sprintf('plotp0_%i',i)
-load('plotp0_1')
-
-% Go through output_tester for each n and plot every p0
 unstablep0 = [];
 stablep0 = [];
-for n = 1:100
-    
-    for m = 1:199
+
+for i = 1:10
+
+    % Load the data method 1
+    % filename = sprintf('plotp0_%i',i)
+
+    % Load the data method 2
+    filename = sprintf('plotp02_%i',i)
+
+    load(filename)
+    % load('plotp0_1')
+
+    % Go through output_tester for each n and plot every p0
+    % unstablep0 = [];
+    % stablep0 = [];
+    for n = 1:100
+
+        % Keep track of whether rod stayed stable whole time
+        unstablenow = [];
+        stablenow = [];
+
+        for m = 1:199
+
+           if ~isempty(output_tester(n,m).tconj)
+               % store that p0 as unstable
+               unstablenow = [unstablenow; output_tester(n,m).startp0];
+           else
+              % store that p0 as stable 
+               stablenow = [stablenow; output_tester(n,m).startp0];
+           end
         
-       if ~isempty(output_tester(n,m).tconj)
-           % store that p0 as unstable
-           unstablep0 = [unstablep0; output_tester(n,m).startp0];
-       else
-          % store that p0 as stable 
-           stablep0 = [stablep0; output_tester(n,m).startp0];
-       end
-        
+        % end of m forloop
+        end
+
+        % Only store rods that underwent an instability
+        if ~isempty(unstablenow)
+            unstablep0 = [unstablep0; unstablenow];
+            stablep0 = [stablep0; stablenow];
+        end
+
+    % end of n forloop
     end
-    
+
+% end of i forloop
 end
 
 % plot it all at the end
@@ -38,8 +63,13 @@ if ~isempty(stablep0)
 plot3(stablep0(:,1),stablep0(:,2),...
              stablep0(:,3), '.b')
 end
+
 % title method 1
-title('p0 paths')
+% title('p0 paths')
+
+% title method 2
+title('p0 paths method 2')
+
 legend('unstable p0','stable p0','Location', 'Best')
 xlabel('p_1')
 ylabel('p_2')
