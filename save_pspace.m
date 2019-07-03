a3dd1f5f4c905854e8ed8f04f4305ef8b5d76e4e
DebraChait <33737987+DebraChait@users.noreply.github.com>
2019-07-03 15:08:43 -0400

    Changed name, added save, removed plot

--- save_pspace.m
+++ save_pspace.m
@@ -46,27 +46,10 @@ for i = 0:5:10
     % end of j forloop
     end
     
-% plot it all at the end for each w
-figure(i+1)
-view(3)
-hold on
-if ~isempty(stablep0)
-plot3(stablep0(:,1),stablep0(:,2),...
-             stablep0(:,3), '.b')
-end
-if ~isempty(unstablep0)
-plot3(unstablep0(:,1), unstablep0(:,2), ...
-    unstablep0(:,3), '.r')
-end
-% title method 1
-name = sprintf('p0 space, w = %i, range = %i,%i,%i',i,...
-    xrange,yrange,zrange)
-title(name)
-
-legend('stable p0','unstable p0','Location', 'Best')
-xlabel('p_1')
-ylabel('p_2')
-zlabel('p_3')
+% save data for plotting
+filename = sprintf('pspacedata_w%i_range%i%i%i',w,xrange,...
+    yrange,zrange);
+save(filename,'unstablep0','stablep0');
 
 % end of i forloop
 end
