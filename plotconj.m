3f85ac997725a1551e153e5a3724ee7f2f073aec
Debra Chait <dnchait@gmail.com>
2019-07-11 11:36:57 -0400

    fixed last for plotconj

--- plotconj.m
+++ plotconj.m
@@ -11,78 +11,83 @@ for i = 1:10
     % filename = sprintf('plotp0_%i',i)
     
     % Load the data method 2
-    filename = sprintf('plotp02_%i',i)
+    filename = sprintf('plotp02extgood_%i',i)
     
-    load(filename) 
+    load(filename,'output_tester')
     
     % Within each file, go through output_tester.tconj(n) for every n
     
     for n = 1:100
-    
+        
         % If we want to make this more efficient, determine last entry
         for m = 1:199
             
-           if ~isempty(output_tester(n,m).tconj)
-%               fprintf('n = %i, m = % i \n',n,m)
-%               disp(output_tester(n,m).tconj) 
-              conjstart = [conjstart; output_tester(n,1).startp0];
-              
-              % Get endp0
-              last = 199;
-              for j = 1:199
-                 if isempty(output_tester(n,200-j).endp0)
-                     last = last-1;
-                 elseif ~isempty(output_tester(n,200-j).endp0)
-                     break
-                 end
-              end
-              conjend = [conjend; output_tester(n,last).endp0];
-              
-              % We know it has a conj point, so mark it as conj and break
-              break
-               
-           end
-           
-           if m == 199
-              noconjstart = [noconjstart; output_tester(n,1).startp0];
-              
-              % Get endp0
-              last = 199;
-              for j = 1:199
-                 if isempty(output_tester(n,200-j).endp0)
-                     last = last-1;
-                 elseif ~isempty(output_tester(n,200-j).endp0)
-                     break
-                 end
-              end
-              noconjend = [noconjend; output_tester(n,last).endp0];
-           end
+            if ~isempty(output_tester(n,m).tconj)
+                %               fprintf('n = %i, m = % i \n',n,m)
+                %               disp(output_tester(n,m).tconj)
+                conjstart = [conjstart; output_tester(n,1).startp0];
+                
+                % Get endp0
+                last = 199;
+                for j = 1:199
+                    if isempty(output_tester(n,200-j).endp0)
+                        last = 200-j-1;
+                    elseif ~isempty(output_tester(n,200-j).endp0)
+                        break
+                    end
+                    if j == 199
+                        last = 1;
+                    end
+                end
+                conjend = [conjend; output_tester(n,last).endp0];
+                
+                % We know it has a conj point, so mark it as conj and break
+                break      
+            end
+            
+            % if m == 199, stable throughout
+            if m == 199
+                noconjstart = [noconjstart; output_tester(n,1).startp0]; 
+                % Get endp0
+                last = 199;
+                for j = 1:199
+                    if isempty(output_tester(n,200-j).endp0)
+                        last = 200-j-1;
+                    elseif ~isempty(output_tester(n,200-j).endp0)
+                        break
+                    end
+                    if j == 199
+                        last = 1;
+                    end
+                end
+                noconjend = [noconjend; output_tester(n,last).endp0];
+            end
             
         end
-    
+        
     end
     
-
+    
 end
-
+conjstart
 figure(1)
 view(3)
 hold on
-if ~isempty(conjstart)
-plot3(conjstart(:,1), conjstart(:,2), ...
-    conjstart(:,3), 'or')
-end
 if ~isempty(noconjstart)
-plot3(noconjstart(:,1),noconjstart(:,2),...
-             noconjstart(:,3), 'ob')
+    plot3(noconjstart(:,1),noconjstart(:,2),...
+        noconjstart(:,3), 'ob')
+end
+if ~isempty(conjstart)
+    plot3(conjstart(:,1), conjstart(:,2), ...
+        conjstart(:,3), 'or')
 end
 % title method 1
 % title('start p0s conj/noconj')
 
 % title method 2
-title('start p0s conj/noconj method 2')
+title('start p0s ext conj/noconj method 2')
 
-legend('conjstart','noconjstart','Location', 'Best')
+legend('noconjstart','conjstart','Location', 'Best')
 xlabel('p_1(0)')
 ylabel('p_2(0)')
 zlabel('p_3(0)')
@@ -91,19 +96,19 @@ figure(2)
 view(3)
 hold on
 if ~isempty(conjend)
-plot3(conjend(:,1), conjend(:,2), ...
-    conjend(:,3), 'or')
+    plot3(conjend(:,1), conjend(:,2), ...
+        conjend(:,3), 'or')
 end
 if ~isempty(noconjend)
-plot3(noconjend(:,1),noconjend(:,2),...
-             noconjend(:,3), 'ob')
+    plot3(noconjend(:,1),noconjend(:,2),...
+        noconjend(:,3), 'ob')
 end
 
 % title method 1
 % title('end p0s conj/noconj')
 
 % title method 2
-title('end p0s conj/noconj method 2')
+title('end p0s ext conj/noconj method 2')
 
 legend('conjend','noconjend','Location', 'Best')
 xlabel('p_1(1)')
